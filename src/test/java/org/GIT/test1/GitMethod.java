e86fce4ae5e7bf64fbe2cb19525dd1ecc8b41c1b
RajeswaraThirechanku <rajeswarant2@gmail.com>
2022-05-04 11:15:10 +0530

    Rajeswara program

--- src/test/java/org/GIT/test1/GitMethod.java
+++ src/test/java/org/GIT/test1/GitMethod.java
@@ -1,5 +1,23 @@
 package org.GIT.test1;
 
 public class GitMethod {
+	private void Test1() {
+		System.out.println("1");
+	}
+	private void Test2() {
+		System.out.println("2");
+	}
+	private void Test3() {
+		System.out.println("3");
+	}
+	private void Test4() {
+		System.out.println("4");
+	}
+	private void Test5() {
+		System.out.println("5");
+	}
+	private void Test6() {
+		System.out.println("6");
+	}
 
 }
